35ab8fe3a313e490da8d35deb976a833984d508e
Mohammed Alsayani <mohammed.alsayani@gmail>
2022-08-09 21:11:42 +0300

    update

--- src/app/layouts/admin-layout/apartments/apartments-list/apartments-list.component.ts
+++ src/app/layouts/admin-layout/apartments/apartments-list/apartments-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+import { IApartment } from 'src/app/shared/models/apartment.model';
+import { ApartmentService } from 'src/app/shared/services/apartment.service';
 
 @Component({
   selector: 'app-apartments-list',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./apartments-list.component.css']
 })
 export class ApartmentsListComponent implements OnInit {
+  public loading = false;
+  list: IApartment[] = [];
+  constructor(
+    private _service: ApartmentService
 
-  constructor() { }
+  ) { }
 
   ngOnInit(): void {
+    this.getList();
+
   }
 
+ async getList() {
+   
+    this.list = await (await this._service.getAll({ limit: 100000 })).data;  
+
+  }
 }
